3192d047b57fbe501828ee348af8f9ae2702f3aa
Jeroen van de Sanden <vandesanden@hotmail.com>
2022-06-28 15:45:14 +0200

    Randomly select image from data array

--- src/components/MainContent/MainContent.jsx
+++ src/components/MainContent/MainContent.jsx
@@ -1,15 +1,22 @@
 import Meme from "../Meme/Meme";
+import { memesData } from "../../data/memesData";
 
 export default function MainContent() {
+  function generateMeme() {
+    const memesList = memesData.data.memes;
+    const randomNumber = Math.floor(Math.random() * 100);
+    console.log(memesList[randomNumber].url);
+  }
+
   return (
     <main className="main">
-      <form className="form">
+      <section className="form">
         <fieldset className="form_textInputs">
           <input type="text" id="toptext" placeholder="Shut up" />
           <input type="text" id="bottomtext" placeholder="and take my money" />
         </fieldset>
-        <button type="submit">Get a new meme image &#128444;</button>
-      </form>
+        <button onClick={generateMeme}>Get a new meme image &#128444;</button>
+      </section>
       <Meme />
     </main>
   );
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 // @ts-nocheck
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import Header from "./components/Header/Header";
 import MainContent from "./components/MainContent/MainContent";
 import "./styles/main.css";
