32a0d10bdc95ef1d9d736da982c80fd88b660886
Jeroen van de Sanden <vandesanden@hotmail.com>
2022-06-28 11:39:06 +0200

    feat: Install NPM & Create main section fo the website
    Creation of Form, Header and Meme components with empty functions and without data yet

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
+// @ts-nocheck
 import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "../src/pages/App/App";
+import ReactDOM from "react-dom";
+import Form from "./components/Form/Form";
+import Header from "./components/Header/Header";
+import Meme from "./components/Meme/Meme";
 import "./styles/main.css";
 
 // @ts-ignore
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Header />
+    <Form />
+    <Meme />
   </React.StrictMode>
 );
