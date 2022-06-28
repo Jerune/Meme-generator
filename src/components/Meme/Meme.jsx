2ab41e7025b2d2c1b01ae20a50282bea6806cbd2
Jeroen van de Sanden <vandesanden@hotmail.com>
2022-06-28 14:46:26 +0200

    Create Meme Component with dummy text

--- src/components/Meme/Meme.jsx
+++ src/components/Meme/Meme.jsx
@@ -1,3 +1,11 @@
+import defaultMeme from "../../assets/memeimg.jpg";
+
 export default function Meme() {
-  return <div>Meme</div>;
+  return (
+    <div className="meme">
+      <img src={defaultMeme} alt="default meme" />
+      <h4 className="meme_top-text">Shut Up</h4>
+      <h4 className="meme_bottom-text">and take my money</h4>
+    </div>
+  );
 }
