54d47a03e64a2d241c47ed54d1c12b9a967d2e62
Jeroen van de Sanden <vandesanden@hotmail.com>
2022-06-28 15:03:59 +0200

    refactor: Refactoring of MainContent component
    Moved Form component to be included into MainContent to make state management easier afterwards

--- src/components/MainContent/MainContent.jsx
+++ src/components/MainContent/MainContent.jsx
@@ -1,10 +1,15 @@
-import Form from "../Form/Form";
 import Meme from "../Meme/Meme";
 
 export default function MainContent() {
   return (
     <main className="main">
-      <Form />
+      <form className="form">
+        <fieldset className="form_textInputs">
+          <input type="text" id="toptext" placeholder="Shut up" />
+          <input type="text" id="bottomtext" placeholder="and take my money" />
+        </fieldset>
+        <button type="submit">Get a new meme image &#128444;</button>
+      </form>
       <Meme />
     </main>
   );
